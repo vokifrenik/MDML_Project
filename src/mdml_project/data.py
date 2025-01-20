900bd9a2980fdecb025c1b9d1ba03ae9e41cbcb0
Viktor Milenov <v.milenov@student.rug.nl>
2025-01-20 12:30:54 +0100

    data

--- src/mdml_project/data.py
+++ src/mdml_project/data.py
@@ -1,29 +1,11 @@
-from pathlib import Path
-
-import typer
-from torch.utils.data import Dataset
+import json
+import pandas as pd
 
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+def load_data():
+    # load a json file
+    train = pd.read_json('data/processed/train.json')
+    test = pd.read_json('data/processed/test.json')
 
-    def __init__(self, raw_data_path: Path) -> None:
-        self.data_path = raw_data_path
-
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
-
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
-
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
-
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(raw_data_path)
-    dataset.preprocess(output_folder)
-
-
-if __name__ == "__main__":
-    typer.run(preprocess)
+    print('Data loaded successfully')
+    return train, test
---
+++ src/mdml_project/model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from data import load_data
+
+
+train, test = load_data()
+print(train.head())
+
