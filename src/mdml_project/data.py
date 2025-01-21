5e92ecd929941144ccd784943076860e9d4108d0
Viktor Milenov <v.milenov@student.rug.nl>
2025-01-21 12:43:15 +0100

    data loader and trial model

--- src/mdml_project/data.py
+++ src/mdml_project/data.py
@@ -1,5 +1,11 @@
 import json
 import pandas as pd
+import numpy as np
+from ase import Atoms
+from dscribe.descriptors import CoulombMatrix
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
 
 
 def load_data():
@@ -7,5 +13,52 @@ def load_data():
     train = pd.read_json('data/processed/train.json')
     test = pd.read_json('data/processed/test.json')
 
+    train.atoms = train.atoms.apply(lambda x: Atoms(**x)) # OBS This one is important!
+    test.atoms = test.atoms.apply(lambda x: Atoms(**x))
+
+    species = []
+    number_of_atoms = []
+    atomic_numbers = []
+    for atom in pd.concat([train.atoms,test.atoms]):
+        species = list(set(species+atom.get_chemical_symbols()))
+        atomic_numbers = list(set(atomic_numbers+list(atom.get_atomic_numbers())))
+        number_of_atoms.append(len(atom))
+
+    max_number_of_atoms = np.max(number_of_atoms)
+    min_atomic_number = np.min(atomic_numbers)
+    max_atomic_number = np.max(atomic_numbers)
+
+    print(len(species))
+
+    # Setting up the CM descriptor
+    cm = CoulombMatrix(
+        n_atoms_max=max_number_of_atoms,
+    )
+
+    cmats = np.zeros((len(train),max_number_of_atoms**2))
+    for i,atoms in enumerate(train.atoms):
+        if i%1000 == 0:
+            print(i)
+        cmats[i,:] = cm.create(atoms)
+    print(len(cmats))
+
+    X = pd.DataFrame(data = cmats, index=train.id)
+    y = train['hform']
+    print('X: {}'.format(X.shape))
+    print('y: {}'.format(y.shape))
+
+    # Standardize the data
+    #scaler = StandardScaler()
+    #X = scaler.fit_transform(X)
+
+    # PCA
+    pca = PCA(n_components=100)
+    X = pca.fit_transform(X)
+
+    print('X: {}'.format(X.shape)) 
+    print('y: {}'.format(y.shape))
+
+    #y = y.values
+
     print('Data loaded successfully')
-    return train, test
+    return X, y
--- src/mdml_project/model.py
+++ src/mdml_project/model.py
@@ -1,7 +1,36 @@
-import pandas as pd
+import numpy as np
 from data import load_data
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, Matern
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
+
+def rmse(y_true, y_pred):
+    return np.sqrt(np.mean((y_true - y_pred)**2))
 
 train, test = load_data()
-print(train.head())
+
+X_train, X_test, y_train, y_test = train_test_split(train, test, test_size=0.2, random_state=42)
+
+y_test = y_test.values
+y_train = y_train.values
+
+kernel = RBF(length_scale=1.0) + Matern(length_scale=1.0, nu=1.5)
+gp = GaussianProcessRegressor(kernel=kernel, alpha=1e-6)
+gp.fit(X_train, y_train)
+
+y_pred = gp.predict(X_test)
+
+# print accuracy
+print(rmse(y_test, y_pred))
+
+
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+y_pred = lm.predict(X_test)
+
+# print accuracy
+print(rmse(y_test, y_pred))
+
 
